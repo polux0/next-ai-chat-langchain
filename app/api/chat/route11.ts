6e6b65ae8ca91d7753b009e37a0939e00cc759b5
polux0 <afidelo@protonmail.com>
2023-08-23 16:07:58 +0200

    fix: commented out unused function;

--- app/api/chat/route11.ts
+++ app/api/chat/route11.ts
@@ -1,31 +1,31 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai'
-import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { AIMessage, HumanMessage } from 'langchain/schema'
+// import { StreamingTextResponse, LangChainStream, Message } from 'ai'
+// import { ChatOpenAI } from 'langchain/chat_models/openai'
+// import { AIMessage, HumanMessage } from 'langchain/schema'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
-export async function POST(req: Request) {
+// export async function POST(req: Request) {
 
   
-  const { messages } = await req.json()
+//   const { messages } = await req.json()
 
-  const { stream, handlers } = LangChainStream()
+//   const { stream, handlers } = LangChainStream()
 
-  const llm = new ChatOpenAI({
-    streaming: true
-  })
+//   const llm = new ChatOpenAI({
+//     streaming: true
+//   })
 
-  llm
-    .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      ),
-      {},
-      [handlers]
-    )
-    .catch(console.error)
+//   llm
+//     .call(
+//       (messages as Message[]).map(m =>
+//         m.role == 'user'
+//           ? new HumanMessage(m.content)
+//           : new AIMessage(m.content)
+//       ),
+//       {},
+//       [handlers]
+//     )
+//     .catch(console.error)
 
-  return new StreamingTextResponse(stream)
-}
+//   return new StreamingTextResponse(stream)
+// }
